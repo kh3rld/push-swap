055c7f76b8cb3a2dbd09d43def22da75210d2014
kherldhussein <eternety11@gmail.com>
2024-08-18 12:21:27 +0300

    Feat(main): Call the validator to test sort Stack

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
+	sort "swap/sortStack"
 	valid "swap/validator"
 )
 
 func main() {
-	s := "1234"
-	fmt.Println(valid.Validate(s))
+	arg := os.Args[1]
+	if valid.Validate(arg) {
+		fmt.Println(arg)
+		for _, i := range arg {
+			k := int(i)
+			data := sort.Stack{Data: []int{k}}
+			data.Push(1)
+		}
+	} else {
+		fmt.Println("Error")
+	}
 }
