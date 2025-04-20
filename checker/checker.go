1217eff1dd864e614a225562a03afaf54d92b960
kh3rld <kherld.hussein@gmail.com>
2025-04-19 22:44:13 -0400

    refactor(checker): simplify instruction execution and stack management in checker

--- checker/checker.go
+++ checker/checker.go
@@ -6,86 +6,111 @@ import (
 	"os"
 	"strconv"
 	"strings"
-
-	"swap/sortStack"
-	"swap/validator"
 )
 
-func executeInstructions(a, b *sortStack.Stack, instructions []string) bool {
-	for _, instruction := range instructions {
-		switch instruction {
-		case "pa":
-			sortStack.Pa(a, b)
-		case "pb":
-			sortStack.Pb(a, b)
-		case "sa":
-			sortStack.Sa(a)
-		case "sb":
-			sortStack.Sb(b)
-		case "ss":
-			sortStack.Ss(a, b)
-		case "ra":
-			sortStack.Ra(a)
-		case "rb":
-			sortStack.Rb(b)
-		case "rr":
-			sortStack.Rr(a, b)
-		case "rra":
-			sortStack.Rra(a)
-		case "rrb":
-			sortStack.Rrb(b)
-		case "rrr":
-			sortStack.Rrr(a, b)
-		default:
-			fmt.Fprintln(os.Stderr, "Error")
-			return false
-		}
-	}
-	return sortStack.IsSorted(a) && len(b.Data) == 0
+type Stack struct {
+	A []int
+	B []int
 }
 
 func main() {
 	if len(os.Args) < 2 {
 		return
 	}
+
+	// Parse input stack A
 	arg := os.Args[1]
-
-	if !validator.Validate(arg) {
-		fmt.Fprintln(os.Stderr, "Error")
-		return
-	}
-
 	values := strings.Split(arg, " ")
-	a := sortStack.NewStack()
-	b := sortStack.NewStack()
-
+	stack := &Stack{A: []int{}, B: []int{}}
 	for _, v := range values {
-		data, err := strconv.Atoi(v)
+		num, err := strconv.Atoi(v)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error")
 			return
 		}
-		a.Push(data)
+		stack.A = append(stack.A, num)
 	}
 
+	// Read instructions from standard input
 	scanner := bufio.NewScanner(os.Stdin)
-	var instructions []string
+	instructions := []string{}
 	for scanner.Scan() {
-		line := scanner.Text()
-		if line == "" {
-			break
+		instructions = append(instructions, scanner.Text())
+	}
+
+	// Execute instructions
+	for _, instr := range instructions {
+		switch instr {
+		case "sa":
+			swap(&stack.A)
+		case "sb":
+			swap(&stack.B)
+		case "ss":
+			swap(&stack.A)
+			swap(&stack.B)
+		case "pa":
+			push(&stack.B, &stack.A)
+		case "pb":
+			push(&stack.A, &stack.B)
+		case "ra":
+			rotate(&stack.A)
+		case "rb":
+			rotate(&stack.B)
+		case "rr":
+			rotate(&stack.A)
+			rotate(&stack.B)
+		case "rra":
+			reverseRotate(&stack.A)
+		case "rrb":
+			reverseRotate(&stack.B)
+		case "rrr":
+			reverseRotate(&stack.A)
+			reverseRotate(&stack.B)
+		default:
+			fmt.Fprintln(os.Stderr, "Error")
+			return
 		}
-		instructions = append(instructions, line)
 	}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "Error")
-		return
-	}
-
-	if executeInstructions(a, b, instructions) {
+	// Check if stack A is sorted and stack B is empty
+	if isSorted(stack.A) && len(stack.B) == 0 {
 		fmt.Println("OK")
 	} else {
 		fmt.Println("KO")
 	}
+}
+
+// Helper functions
+func swap(stack *[]int) {
+	if len(*stack) > 1 {
+		(*stack)[0], (*stack)[1] = (*stack)[1], (*stack)[0]
+	}
+}
+
+func push(from *[]int, to *[]int) {
+	if len(*from) > 0 {
+		*to = append([]int{(*from)[0]}, *to...)
+		*from = (*from)[1:]
+	}
+}
+
+func rotate(stack *[]int) {
+	if len(*stack) > 0 {
+		*stack = append((*stack)[1:], (*stack)[0])
+	}
+}
+
+func reverseRotate(stack *[]int) {
+	if len(*stack) > 0 {
+		*stack = append([]int{(*stack)[len(*stack)-1]}, (*stack)[:len(*stack)-1]...)
+	}
+}
+
+func isSorted(stack []int) bool {
+	for i := 1; i < len(stack); i++ {
+		if stack[i-1] > stack[i] {
+			return false
+		}
+	}
+	return true
 }
