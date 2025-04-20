cf1c85fa795ac4a3631acccb929fce94ad91356d
doonyango <datieno001@gmail.com>
2025-04-20 14:45:48 +0300

    feat: implement function to execute the instructions

--- validator/exec_intructions.go
+++ validator/exec_intructions.go
@@ -11,3 +11,34 @@ func IsValideInstructions(instruction string) bool {
 	_, ok := validInstructions[instruction]
 	return ok
 }
+
+// execute instructions
+func ExecuteInstruction(instruction string, a, b *StackList) {
+	switch instruction {
+	case "sa":
+		SwitchFirstTwo(a, "a")
+	case "sb":
+		SwitchFirstTwo(b, "b")
+	case "ss":
+		SwitchFirstTwo(a, "a")
+		SwitchFirstTwo(b, "b")
+	case "pa":
+		PushToStack(a, b, "a")
+	case "pb":
+		PushToStack(b, a, "b")
+	case "ra":
+		RotateStack(a, "a")
+	case "rb":
+		RotateStack(b, "b")
+	case "rr":
+		RotateStack(a, "a")
+		RotateStack(b, "b")
+	case "rra":
+		ReverseRotateStack(a, "a")
+	case "rrb":
+		ReverseRotateStack(b, "b")
+	case "rrr":
+		ReverseRotateStack(a, "a")
+		ReverseRotateStack(b, "b")
+	}
+}
