d3d4f2894badf2c1c85e11cd7afc25fbed834a8e
doonyango <datieno001@gmail.com>
2025-05-10 13:33:06 +0300

    refactor: rename directory and folder to align with the requirements

---
+++ pushswap/push-swap.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	pushswap "swap/validator"
+)
+
+func main() {
+	args := os.Args[1:]
+	values, err := pushswap.ParseArgs(args)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	if values == nil {
+		return
+	}
+
+	stack := pushswap.NewStackList()
+	for _, val := range values {
+		stack.Push(val)
+	}
+
+	if !stack.IsSorted() {
+		switch stack.Length() {
+		case 2:
+			pushswap.SwitchFirstTwo(stack, "a")
+		case 3:
+			pushswap.TinySort(stack)
+		default:
+			pushswap.Sort_stack(stack)
+		}
+	}
+
+	fmt.Println(strings.Join(pushswap.Moves, "\n"))
+}
